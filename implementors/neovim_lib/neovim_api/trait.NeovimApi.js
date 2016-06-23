4ddeb8612fb9a8bed717fa48d3d2148d832fe2d8
daa84 <daa84@inbox.ru>
2016-06-23 16:17:02 +0300

    Api update to new version

--- implementors/neovim_lib/neovim_api/trait.NeovimApi.js
+++ implementors/neovim_lib/neovim_api/trait.NeovimApi.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['neovim_lib'] = [];
+implementors['neovim_lib'] = [];implementors['neovim_lib'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
